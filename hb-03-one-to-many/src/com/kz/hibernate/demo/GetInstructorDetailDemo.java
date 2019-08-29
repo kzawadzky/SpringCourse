9e2ec48606f469ebf7ce0744de73787f8275fd15
=Krzysztof Zawadzki <zawadzki1990@gmail.com>
2019-08-29 12:57:52 +0200

    one to many mapping

---
+++ hb-03-one-to-many/src/com/kz/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,55 @@
+package com.kz.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.kz.hibernate.demo.entity.Instructor;
+import com.kz.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start transaction
+			session.beginTransaction();
+
+			// get the instructor detail
+			int theId = 288;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+			//print instructor detail
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+			
+			//print the associated instructor
+			System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
+			
+			
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("DONE.");
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+		
+		finally {
+			session.close();
+			
+			factory.close();
+		}
+
+	}
+
+}
